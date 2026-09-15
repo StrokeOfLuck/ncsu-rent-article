30c3b31603970760343b469a318f8b44e3717c0e
Sean Ryan <92698057+StrokeOfLuck@users.noreply.github.com>
2026-09-15 15:23:37 -0400

    Load Pete Fraccaroli and Thomas Barrie references

--- assets/reference-math.js
+++ assets/reference-math.js
@@ -1,3 +1,3 @@
 (function(){
-  document.write('<script src="assets/reference-math-core.js?v=20260915-winter-break"><\/script><script src="assets/winter-break-reference.js?v=20260915-winter-break"><\/script><script src="assets/donna-mcgalliard-reference.js?v=20260915-donna"><\/script>');
+  document.write('<script src="assets/reference-math-core.js?v=20260915-winter-break"><\/script><script src="assets/winter-break-reference.js?v=20260915-winter-break"><\/script><script src="assets/donna-mcgalliard-reference.js?v=20260915-donna"><\/script><script src="assets/pete-barrie-references.js?v=20260915-pete-barrie"><\/script>');
 })();
